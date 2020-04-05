9212c583b0eb75a77e66fe42dc9bc4be17b31475
matthewhuntdev <matthewhuntdev@gmail.com>
2020-04-05 16:03:26 +0100

    Updates

---
+++ precache-manifest.220045ed376e999666c05367987b16dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83286a560c6176aec4794223010edfe1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1271902dc05b3a7b4c76",
+    "url": "/monsters-rolodex/static/css/main.83cd3215.chunk.css"
+  },
+  {
+    "revision": "c1ace85d8131c29820d2",
+    "url": "/monsters-rolodex/static/js/2.8f057a9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8f057a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1271902dc05b3a7b4c76",
+    "url": "/monsters-rolodex/static/js/main.10aa36cc.chunk.js"
+  },
+  {
+    "revision": "68b00582aea0f61137cd",
+    "url": "/monsters-rolodex/static/js/runtime-main.4c13341c.js"
+  }
+]);
